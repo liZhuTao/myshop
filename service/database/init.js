64ccd921f55f8d542590b297b505452d47252329
liZhuTao <1260051574@qq.com>
2020-03-05 22:26:00 +0800

    搭建服务器

--- service/database/init.js
+++ service/database/init.js
@@ -1,13 +1,42 @@
 const mongoose = require('mongoose')
 const db = "mongodb://localhost/smile-db"
 
-exports.connect = ()=>{
-    //链接数据库
-    mongoose.connect(db)
-    
+exports.connect = () => {
+  //链接数据库
+  mongoose.connect(db)
+  let maxConnectTime = 0;
+
+  return new Promise((resolve, reject) => {
     //增加数据库的监听事件
-    mongoose.connection.on('disconnection',()=>{
-        console.log("*******数据库断开********")
-        mongoose.connect(db)
+    mongoose.connection.on('disconnection', () => {
+      console.log("**************数据库断开**************")
+      if(maxConnectTime<=3){
+        maxConnectTime++;
+        mongoose.connect(maxConnectTime);
+      }else{
+          reject()
+          throw new Error('数据库出现问题，程序无法搞定，请人为处理......')
+      }
+      
     })
+
+    mongoose.connection.on('error', (err) => {
+      console.log("**************数据库错误**************")
+      if(maxConnectTime<=3){
+        maxConnectTime++;
+        mongoose.connect(maxConnectTime);
+      }else{
+          reject(err)
+          throw new Error('数据库出现问题，程序无法搞定，请人为处理......')
+      }
+    })
+
+    //链接打开时
+    mongoose.connection.once('open', () => {
+      console.log("MongoDB connected successfully");
+      resolve()
+    })
+  })
+
+
 }
