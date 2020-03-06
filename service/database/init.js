f3d2fe9ea8b0ab8117c17c4bf735f0dcf7cc8f64
liZhuTao <1260051574@qq.com>
2020-03-06 14:56:55 +0800

    数据库操作

--- service/database/init.js
+++ service/database/init.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose')
-const db = "mongodb://localhost/smile-db"
+const db = "mongodb://localhost/smile-vue"
+const glob = require('glob')
+const {resolve} = require('path')
+
+exports.initSchemas = () =>{
+  glob.sync(resolve(__dirname,'./schema/','**/*.js')).forEach(require)
+}
 
 exports.connect = () => {
   //链接数据库
--- service/index.js
+++ service/index.js
@@ -1,15 +1,33 @@
 const Koa = require('koa')
 const app = new Koa()
-const { connect } = require('./database/init.js')
+const mongoose = require('mongoose')
+const {
+  connect,
+  initSchemas
+} = require('./database/init.js')
 
-;(async()=>{
-    await connect()
+;(async () => {
+  await connect()
+  initSchemas()
+  const User = mongoose.model('User')
+  let oneUser = new User({
+    userName: 'jsp02',
+    password: '123456'
+  })
+  oneUser.save().then(() => {
+    console.log('插入成功')
+  })
+  let users = await User.findOne({}).exec()
+  console.log('------------------')
+  console.log(users)
+  console.log('------------------')
+
 })()
 
-app.use(async(ctx)=>{
-    ctx.body = '<h1>hello Koa2</h1>'
+app.use(async (ctx) => {
+  ctx.body = '<h1>hello Koa2</h1>'
 })
 
-app.listen(3000,()=>{
-    console.log('[Server] starting at port 3000')
+app.listen(3000, () => {
+  console.log('[Server] starting at port 3000')
 })
